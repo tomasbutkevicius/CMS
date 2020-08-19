01a9dc89d3013aebd6455050be87d312fd88c6b2
tomasbutkevicius <tomas.butkevicius99@gmail.com>
2020-08-19 21:29:26 +0300

    feat: more fields/better validation

--- script.js
+++ script.js
@@ -2,30 +2,38 @@ const tableKey = 'cms-table';
 
 let cmsTable;
 let cmsTableDemo = {
-    'JohnBob': {
-        'firstName': 'John',
-        'lastName': 'Bob',
-        'phone': '111111',
-        'address': '4444 SankPeter'
+    'AlgirdasKuzminskas': {
+        'firstName': 'Algirdas',
+        'lastName': 'Kuzminskas',
+        'phone': '864599877',
+        'email': 'algis@gmail.com',
+        'birthDate': '1999-07-18',
+        'address': 'Saint Petersburg'
     },
-    'JohnBob': {
+    'RupertGimm': {
         'firstName': 'Rupert',
         'lastName': 'Gimm',
+        'birthDate': '1999-05-05',
         'phone': '448484',
+        'email': 'rup@gmail.com',
         'address': 'st Barbara street'
     }
 };
 
-// let enableDisableNameInput = (option)=>{
-//     let newPersonNumber = document.getElementById('newPersonName');
-//     if(option === 'enable'){
-//         newPersonName.disabled = false;
-//     } else if(option === 'disable'){
-//         newPersonName.disabled = true;
-//     }
-// }
+let enableDisableNameInput = (option)=>{
+    personFirstName = document.getElementById("newPersonFirstName");
+    personLastName = document.getElementById("newPersonLastName");
+    if(option === 'enable'){
+        personFirstName.disabled = false;
+        personLastName.disabled = false;
+    } else if(option === 'disable'){
+        personFirstName.disabled = true;
+        personLastName.disabled = true;
+    }
+}
 
 let refreshDOMTable = () => {
+    let editingEnabled = false;
     let cmsTableKeys = Object.keys(cmsTable); // ['Giggle Kible', 'Merry Jingles']
     let tableContainer = document.getElementById("cmsTableContainer");
     let oldTableBody = document.getElementById('tableBody');
@@ -40,7 +48,9 @@ let refreshDOMTable = () => {
         let currentNumberCol = document.createElement('div');
         let currentFirstNameCol = document.createElement('div');
         let currentLastNameCol = document.createElement('div');
+        let currentBirthDateCol = document.createElement('div');
         let currentPhoneCol = document.createElement('div');
+        let currentEmailCol = document.createElement('div');
         let currentAddressCol = document.createElement('div');
         let currentEditBtn = document.createElement('div');
         let currentDeleteBtn = document.createElement('div');
@@ -49,7 +59,9 @@ let refreshDOMTable = () => {
         currentNumberCol.className = 'cms-table-column cms-number';
         currentFirstNameCol.className = 'cms-table-column cms-first-name';
         currentLastNameCol.className = 'cms-table-column cms-last-name';
+        currentBirthDateCol.className = 'cms-table-column cms-birth-date';
         currentPhoneCol.className = 'cms-table-column cms-phone';
+        currentEmailCol.className = 'cms-table-column cms-email'
         currentAddressCol.className = 'cms-table-column cms-address';
         currentEditBtn.className ='cms-table-column cms-edit';
         currentDeleteBtn.className = 'cms-table-column cms-delete';
@@ -57,8 +69,12 @@ let refreshDOMTable = () => {
         currentNumberCol.innerHTML = cmsTableKeys[i];
         currentFirstNameCol.innerHTML = cmsTable[cmsTableKeys[i]].firstName;
         currentLastNameCol.innerHTML = cmsTable[cmsTableKeys[i]].lastName;
+        currentBirthDateCol.innerHTML = cmsTable[cmsTableKeys[i]].birthDate;
+
         currentPhoneCol.innerHTML = cmsTable[cmsTableKeys[i]].phone;
+        currentEmailCol.innerHTML = cmsTable[cmsTableKeys[i]].email;
         currentAddressCol.innerHTML = cmsTable[cmsTableKeys[i]].address;
+        
 
         currentDeleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
         currentEditBtn.innerHTML = '<i class="fas fa-edit"></i>';
@@ -66,7 +82,9 @@ let refreshDOMTable = () => {
         currentRow.appendChild(currentNumberCol);
         currentRow.appendChild(currentFirstNameCol);
         currentRow.appendChild(currentLastNameCol);
+        currentRow.appendChild(currentBirthDateCol);
         currentRow.appendChild(currentPhoneCol);
+        currentRow.appendChild(currentEmailCol);
         currentRow.appendChild(currentAddressCol);
         currentRow.appendChild(currentEditBtn);
         currentRow.appendChild(currentDeleteBtn);
@@ -79,8 +97,12 @@ let refreshDOMTable = () => {
         newPersonFirstName.value = '';
         let newPersonLastName = document.getElementById('newPersonLastName'); 
         newPersonLastName.value = '';
+        let newPersonBirthDate = document.getElementById('newPersonBirthDate'); 
+        newPersonBirthDate.value = '';
         let newPersonPhone = document.getElementById('newPersonPhone');
         newPersonPhone.value = '';
+        let newPersonEmail = document.getElementById('newPersonEmail');
+        newPersonEmail.value = '';
         let newPersonAddress = document.getElementById('newPersonAddress');
         newPersonAddress.value = '';
         
@@ -109,12 +131,16 @@ let refreshDOMTable = () => {
 
     //ADD
     newPersonSubmitBtn.addEventListener('click', () => {
-        
         let newPersonFirstName = document.getElementById('newPersonFirstName').value.trim();
         let newPersonLastName = document.getElementById('newPersonLastName').value.trim();
         let newPersonNumber = newPersonFirstName + newPersonLastName;
+        let newPersonBirthDate = document.getElementById('newPersonBirthDate').value.trim();
         let newPersonPhone = document.getElementById('newPersonPhone').value.trim();
+        let newPersonEmail = document.getElementById('newPersonEmail').value.trim();
         let newPersonAddress = document.getElementById('newPersonAddress').value.trim();
+        let errorMessage = document.getElementById("errorMessage");
+        let isValidEmail = false;
+        let isValidPhone = false;
 
         if(newPersonFirstName === ''){
             document.getElementById('newPersonFirstName').className = 'input-err';
@@ -127,6 +153,12 @@ let refreshDOMTable = () => {
         } else {
             document.getElementById('newPersonLastName').className = '';
         }
+        
+        if(newPersonBirthDate === ''){
+            document.getElementById('newPersonBirthDate').className = 'input-err';
+        } else {
+            document.getElementById('newPersonBirthDate').className = '';
+        }
 
         if(newPersonPhone === ''){
             document.getElementById('newPersonPhone').className = 'input-err';
@@ -134,18 +166,52 @@ let refreshDOMTable = () => {
             document.getElementById('newPersonPhone').className = '';
         }
 
-        if(newPersonAddress === ''){
-            document.getElementById('newPersonAddress').className = 'input-err';
+        if(newPersonEmail === ''){
+            document.getElementById('newPersonEmail').className = 'input-err';
         } else {
-            document.getElementById('newPersonAddress').className = '';
+            document.getElementById('newPersonEmail').className = '';
         }
 
-        if(newPersonFirstName !== '' && newPersonLastName !== '' && newPersonPhone !== '' && newPersonAddress !== ''){
-            let newPerson = {};
+        if (validateEmail(newPersonEmail)) {
+            isValidEmail = true;
+            errorMessage.textContent = '';
+            document.getElementById('newPersonEmail').className = ''; 
+            if (!editingEnabled && isEmailAlreadyUsed(newPersonEmail)){
+                    isValidEmail = false;
+                    errorMessage.textContent = 'Email is already used';
+                    document.getElementById('newPersonEmail').className = 'input-err';
+            }
+
+        } else {
+            errorMessage.textContent = 'Email is not valid';
+            document.getElementById('newPersonEmail').className = 'input-err';
+        }
+
+        if (validatePhone(newPersonPhone)) {
+            isValidPhone = true;
+            errorMessage.textContent = '';
+            document.getElementById('newPersonPhone').className = ''; 
+            if (!editingEnabled && isPhoneAlreadyUsed(newPersonPhone)){
+                    isValidPhone = false;
+                    errorMessage.textContent = 'Phone is already used';
+                    document.getElementById('newPersonPhone').className = 'input-err';
+            }
+
+        } else {
+            errorMessage.textContent = 'Phone is not valid';
+            document.getElementById('newPersonPhone').className = 'input-err';
+        }
+
+
+        if(newPersonFirstName !== '' && newPersonLastName !== '' && newPersonPhone !== ''
+         && newPersonEmail !== '' && newPersonBirthDate !== ''
+         && isValidEmail && isValidPhone){
             cmsTable[newPersonNumber] = {
                 'firstName': newPersonFirstName,
                 'lastName': newPersonLastName,
+                'birthDate': newPersonBirthDate,
                 'phone': newPersonPhone,
+                'email': newPersonEmail,
                 'address': newPersonAddress
             }
             localStorage.setItem(tableKey, JSON.stringify(cmsTable));
@@ -156,18 +222,23 @@ let refreshDOMTable = () => {
 
     newPersonCancelBtn.addEventListener('click', () => {
         enableDisableNewUserModal('disable');
+        refreshDOMTable();
     })
 
     addNewEntryBtn.addEventListener('click', () => {
+        enableDisableNameInput('enable');
         enableDisableNewUserModal('enable');
+        editingEnabled = false;
     });
 
 
     //EDIT
     for(let i = 0; i < editBtns.length; i++){
         editBtns[i].addEventListener('click', ($event)=>{
+            editingEnabled = true;
+            let headerText = document.getElementById("newPersonModalHeader");
+            headerText.textContent = "Edit contact";
             let numberToEdit = $event.target.parentElement.children[0].innerText;
-            console.log(numberToEdit);
             let personToEdit = cmsTable[numberToEdit];
             enableDisableNewUserModal('enable'); 
 
@@ -177,13 +248,19 @@ let refreshDOMTable = () => {
             let newPersonLastName = document.getElementById('newPersonLastName'); 
             newPersonLastName.value = personToEdit.lastName;
 
+            let newPersonBirthDate = document.getElementById('newPersonBirthDate'); 
+            newPersonBirthDate.value = personToEdit.birthDate;
+
             let newPersonPhone = document.getElementById('newPersonPhone');
             newPersonPhone.value = personToEdit.phone;
+
+            let newPersonEmail = document.getElementById('newPersonEmail');
+            newPersonEmail.value = personToEdit.email;
 
             let newPersonAddress = document.getElementById('newPersonAddress');
             newPersonAddress.value = personToEdit.address;
            
-            // enableDisableNameInput('disable');
+            enableDisableNameInput('disable');
         })
     }
 
@@ -203,6 +280,17 @@ let refreshDOMTable = () => {
         });
     }
 
+    function validateEmail(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    function validatePhone(phone)
+    {
+        const re = /^[0-9]+$/;
+        return re.test(String(phone).toLowerCase());
+    }
+
     let deleteUserFromTable = (userNumber) => {
         let tempTable = {};
         let cmsTableKeys = Object.keys(cmsTable);
@@ -214,6 +302,28 @@ let refreshDOMTable = () => {
     cmsTable = tempTable;
     localStorage.setItem(tableKey, JSON.stringify(cmsTable));
     refreshDOMTable();
+    }
+
+    let isPhoneAlreadyUsed = (phoneNumber) => {
+        let tempTable = {};
+        let cmsTableKeys = Object.keys(cmsTable);
+        for(let i = 0; i < cmsTableKeys.length; i++){
+            if(phoneNumber === cmsTable[cmsTableKeys[i]].phone){
+                return true;
+            }
+    }
+    return false;
+    }
+
+    function isEmailAlreadyUsed(email) {
+        let tempTable = {};
+        let cmsTableKeys = Object.keys(cmsTable);
+        for(let i = 0; i < cmsTableKeys.length; i++){
+            if(email === cmsTable[cmsTableKeys[i]].email){
+                return true;
+            }
+    }
+    return false;
     }
 }
 
